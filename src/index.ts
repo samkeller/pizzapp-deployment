d9388baab6faf155a095879e6ee5bbdea09aff36
Samuel Keller <keller.samuel1@gmail.com>
2021-04-12 16:42:36 +0200

    Test deploy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "./env";
 import { router } from "./api";
+const path = require('path');
 
 const app = express();
 
@@ -10,16 +11,13 @@ app.use(router);
 
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(__dirname + '/public')); //__dir and not _dir
+const publicPath = path.join(__dirname, '../public')
+app.use(express.static(publicPath)); //__dir and not _dir
 
 app.listen(process.env.PORT, () => {
 console.log(`Server is running on port ${PORT}.`);
 });
 
 router.get("/", (req, res) => {
-    res.sendFile(__dirname+"/public/index.html");
-})
-
-router.get("/salut", (req, res) => {
-    res.status(200).send("salut");
+    res.sendFile(path.join(publicPath, "index.html"));
 })
