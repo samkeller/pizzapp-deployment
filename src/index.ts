bbdcde6aa39ec1ccf172dfab3bcac7b4eee0c603
Samuel Keller <keller.samuel1@gmail.com>
2021-04-14 15:12:32 +0200

    Merge suite

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
-import express from "express";
 import "./env";
-import { router } from "./api";
 import app from './app'
 const path = require('path');
 
 const PORT = process.env.PORT || 8000;
 
-// Front REACT
-app.use(express.static(path.join(__dirname, 'build')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
